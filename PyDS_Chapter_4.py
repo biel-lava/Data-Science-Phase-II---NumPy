cb610fe8e5e97eb54032a261c5c958326343c60b
biel_lava <ralava@up.edu.ph>
2023-10-28 15:55:01 +0800

    Chapter 4 updates

--- PyDS_Chapter_4.py
+++ PyDS_Chapter_4.py
@@ -7,15 +7,27 @@ Topics:
 '''
 
 # NumPy ndarray basics
-
+'''
 import numpy as np
 
 data = np.array([[15, 25, 23],[12, 45, 67]])
 
-#print(data) # simply print the data ndarray
+print(data) # simply print the data ndarray
 
-#print(data*10) # perfoms mathematical operation to the ndarray
+print(data*10) # perfoms mathematical operation to the ndarray
 
-#print(data.shape) # gets the shape of the array
+print(data.shape) # gets the shape of the array
+'''
 
+
+# Transposing Arrays and Swapping Axes
+#'''
+import numpy as np
+
+test_2d = np.array([[42, 13, 44],[63, 74, 93]])
+
+test_swap = test_2d.reshape(2,3)
+print(test_swap)
+
+#'''
 
