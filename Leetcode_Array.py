aecf45c8c5a9962d716cf4b00919a970ee064841
biel_lava <ralava@up.edu.ph>
2023-11-01 17:04:48 +0800

    Solved problem 26

--- Leetcode_Array.py
+++ Leetcode_Array.py
@@ -1,8 +1,32 @@
 '''
-Leetcode Array
+LEETCODE ARRAY
 Date: 10/31/23
 Notes:
     - This is a dedicated file for playing with array problems from Leetcode
     - Basic progression would be puro easy muna then start dealing with the medium difficulties
 '''
+
+
+
+'''
+Problem # 26: Removing Duplicates
+Date: 10/31/23
+Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+Difficulty: Easy
+Notes:  
+    - Make a function to return the number of unique numbers in the array as variable k
+'''
+'''
+sample_array = [0,0,1,1,1,2,2,3,3,4] # test array
+
+def count_dupli(arr):
+    k = 1 # 1 as the default value since kahit isa lang naman yung element sa array considered pa rin yun as 1 unique element
+    for x in range(1, len(sample_array)):
+        if sample_array[x] != sample_array[x-1]: # checks if the current number is similar to the previous array (proper approach since if x+1 mageerror ulit as index out of range)
+            k += 1
+    return k
+print(count_dupli(sample_array))
+'''
+
+
 
--- trial.py
+++ trial.py
@@ -1,5 +1,19 @@
+# Random array generator (integer)
+''''
 import random as rd
 
 test_3d = [rd.randint(1, 100) for x in range(10)]
 
 print(test_3d)
+'''
+
+
+sample_array = [0,0,1,1,1,2,2,3,3,4]
+k = 1
+
+for x in range(1, len(sample_array)):
+    if sample_array[x] != sample_array[x-1]:
+        k += 1
+
+print(k)
+
